911250dcd6478d1fb58e1cdc41a60bf6be37d05c
muhammadkhazil <mkhazil3112@gmail.com>
2021-10-11 10:38:33 +0700

    menambahkan file form

---
+++ tugas harian_3_method_get_dan_request/form_post.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="post.php" method="post">
+        Nama : <input type="text" name="nama"><br>
+        Alamat : <input type="text" name="alamat"> <br>
+        <input type="submit" value="kirim">
+    </form>
+</body>
+
+</html>
---
+++ tugas harian_3_method_get_dan_request/request.php
@@ -0,0 +1,6 @@
+<?php 
+    $nama = $_REQUEST['nama'];
+    $alamat = $_REQUEST['alamat'];
+
+    echo $nama. "<br>";
+    echo $alamat;
